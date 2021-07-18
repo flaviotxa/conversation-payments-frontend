ad6041b65fd26bb165c3ee9fc919565ebc962ecb
Flavio Lopes <flaviotxa@gmail.com>
2021-07-18 18:44:56 +0100

    added template

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,9 @@
 import React from "react";
-import styled from "styled-components";
 
 import Flex from "@cobalt/react-flex";
 import { Heading } from "@cobalt/react-typography";
 import Divider from "@cobalt/react-divider";
 import Tab from "@cobalt/react-tab";
-import { useTheme } from "@cobalt/react-theme-provider";
 import CardList from "./CardList";
 
 export default function Main() {
@@ -16,7 +14,6 @@ export default function Main() {
       active: true,
     },
   ];
-  const theme = useTheme();
   return (
     <>
       <Flex padding="6">
